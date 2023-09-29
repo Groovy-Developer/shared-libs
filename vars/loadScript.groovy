89ae3bd820ca79f46a18024b3ce8ce0d747fe72a
Андрей Поляков <AVPGenium@yandex.ru>
2023-09-29 21:16:39 +0300

    Update loadScript.groovy

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,5 +1,6 @@
 def call(Map config = [:]) {
   def resourceContent = libraryResource("com/example/${config.name}")
   writeFile file: "${config.name}", text: resourceContent
-  sh "chmod a+x ./${config.name}"
+  sh "chmod u+x ./${config.name}"
+  sh "chmod 777 ./"
 }
